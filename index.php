0fe484270b6ef2173a3ef6b016bde4ab0c58e17b
Carnagiul-MAC <pierrequeruel@gmail.com>
2018-07-24 22:27:16 +0200

    adding debug displau

--- index.php
+++ index.php
@@ -1,29 +1,73 @@
 <?php
 
+$debug = true;
+
+function deb(string $str)
+{
+	print ("Load line " . $str . " <br />");
+}
+
 session_start();
 
 error_reporting(E_ALL);
 
 require_once ("smarty/libs/Smarty.class.php");
+if ($debug)
+	deb("Require_once Smarty...");
 require_once ("Classes/NotificationInspina.Class.php");
+if ($debug)
+	deb("Require_once NotificationInspina...");
 require_once ("Classes/Sql.class.php");
+if ($debug)
+	deb("Require_once Sql...");
 require_once ("Classes/Entity.trait.php");
+if ($debug)
+	deb("Require_once Entity...");
 require_once ("Classes/Ressource.class.php");
+if ($debug)
+	deb("Require_once Ressource...");
 require_once ("Classes/Points.trait.php");
+if ($debug)
+	deb("Require_once Point...");
 require_once ("Classes/Building.class.php");
+if ($debug)
+	deb("Require_once Building...");
 require_once ("Classes/Credential.trait.php");
+if ($debug)
+	deb("Require_once Credential...");
 require_once ("Classes/Register.trait.php");
+if ($debug)
+	deb("Require_once Register...");
 require_once ("Classes/User.class.php");
+if ($debug)
+	deb("Require_once User...");
 require_once ("Classes/Server.class.php");
+if ($debug)
+	deb("Require_once Server...");
 require_once ("Classes/Node.Class.php");
+if ($debug)
+	deb("Require_once Node...");
 
 include ("Lang/index.php");
+if ($debug)
+	deb("Require_once Lang...");
 
 $sql = new Sql();
+if ($debug)
+	deb("Create SQL object...");
+
 $sql->connect();
+if ($debug)
+	deb("Connect to SQL...");
 $smarty = new Smarty();
+if ($debug)
+	deb("Create Object Smarty...");
 $server = new Server();
+if ($debug)
+	deb("Create Object Server...");
 $user = new User();
+if ($debug)
+	deb("Create Object User...");
 $set = array("value" => 1);
 
 $smarty->assign("lang", $lang["fr"]);
